e84cfa7fc1afdb469ab041ff6347b6a85f299aee
DiceR011 <touhu0305@gmail.com>
2024-11-29 13:47:14 +0900

    センターポップアップレイアウトの追加

--- src/components/Layout/Layoutcomponents/CenterPopupLayout.tsx
+++ src/components/Layout/Layoutcomponents/CenterPopupLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 interface PopupMenuProps {
     onClose: () => void;
@@ -6,17 +7,23 @@ interface PopupMenuProps {
 }
 
 const CenterPopupLayout: React.FC<PopupMenuProps> = ({ onClose, children }) => {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white rounded-lg p-6 shadow-lg w-80">
+    // ポップアップの中身
+    const popupContent = (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white rounded-lg p-6 shadow-lg w-80 relative">
                 {children}
             </div>
+            {/* 背景をクリックすると閉じる */}
             <div
                 className="absolute inset-0"
                 onClick={onClose}
             ></div>
         </div>
     );
+
+    // ポータルで body 直下にレンダリング
+    return ReactDOM.createPortal(popupContent, document.body);
 };
 
 export default CenterPopupLayout;
+
